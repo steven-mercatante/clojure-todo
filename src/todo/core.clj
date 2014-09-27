75b93decc0d390a57ee9924f36b5157e2fc33f89
Steven Mercatante <steven.mercatante@gmail.com>
2014-09-27 15:09:42 -0400

    Make json-file a standlone var

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -3,20 +3,19 @@
   (:require [clojure.data.json :as json]))
 
 
+(def json-file (str (System/getProperty "user.dir") "/tasks.json"))
+
+
 (defn load-tasks
   "Load tasks from JSON file into memory and convert to map"
   []
-  (let [json-file (str (System/getProperty "user.dir") "/tasks.json")]
-    (json/read-str(slurp json-file) :key-fn keyword)))
+  (json/read-str (slurp json-file) :key-fn keyword))
 
 
 (defn write-tasks
   "Write task list to JSON file"
   [tasks]
-  (println "write tasks")
-  (let [json-file (str (System/getProperty "user.dir") "/tasks.json")]
-    (spit json-file (json/write-str tasks))))
-
+  (spit json-file (json/write-str tasks)))
 
 
 (defn print-tasks
@@ -30,8 +29,6 @@
 (defn add-task
   "Add a task to the list"
   [tasks [task]]
-  ;;(print (conj tasks {:task task :complete false}))
-  ;;(println (str "add task: " task))
   (write-tasks (conj tasks {:task task :complete false})))
 
 
